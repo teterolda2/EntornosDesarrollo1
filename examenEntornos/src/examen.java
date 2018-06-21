2967e2b66e5bdb4e97423d40ad9ff025c3b5a838
alumno <alumno@10.0.2.15>
2018-06-21 11:06:32 +0200

    Version 1.0

---
+++ examenEntornos/src/examen.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Vector;
+/**
+ *
+ * @author alumno
+ */
+public class examen {
+    
+
+public class Cliente {
+
+  public String nombre;
+
+    /**
+   * 
+   * @element-type Pedidos
+   */
+  private Vector  myPedidos;
+
+  public void hacepedidos() {
+  }
+
+}
+
+enum estados {
+
+  pendiente,
+
+  pagado,
+
+  procesando,
+
+  enviado,
+
+  entregado;
+
+}
+enum pago {
+
+  tarjetacredito,
+
+  efectivo,
+
+  moneda;
+
+}
+public class Pedidos {
+
+  private String nombrePedido;
+
+  private String fechaPedido;
+
+  private Integer costePedido;
+
+  private pago tipopago;
+
+  private plazosodirecto pagadoono;
+
+  private estados estado;
+
+    private Cliente myCliente;
+    private Productos myProductos;
+
+  public void pagar() {
+  }
+
+}
+enum plazosodirecto {
+
+  directo,
+
+  plazos;
+
+}
+
+
+public class Productos {
+
+  protected Integer precio;
+
+  protected Integer cantidad;
+
+  protected Integer impuestos;
+
+  protected Integer existencias;
+
+    protected Vector  myPedidos;
+
+  public void calcularCantidad() {
+  }
+
+}
+
+}
